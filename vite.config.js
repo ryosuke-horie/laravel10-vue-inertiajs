473ab7f4a8be9b448ce187adb9dd173a7cc871d9
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-09-20 18:16:31 +0900

    :sparkles: Inertiaのインストール

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        // 外部公開
+        host: '192.168.100.230', // IPを記載
+        port: 5173
+    }
 });
